a37f8601bfb7f7dc84f3f922669aca82355c43a3
AnorldoM23 <arnoldomunoz23@yahoo.com>
2016-08-19 11:05:50 -0700

    Added Aframe and sample code to index.js working code

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
+import 'aframe';
+import {Entity, Scene} from 'aframe-react';
 
-class HelloWorld extends Component {
-  render() {
+class HelloWorld extends React.Component {
+  render () {
     return (
-      <div>Hello mangos</div>
-    )
+      <Scene>
+        <Entity geometry={{primitive: 'box'}} material="color: red" position={[0, 0, -5]}/>
+      </Scene>
+    );
   }
 }
 
