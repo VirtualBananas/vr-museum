01907734e1e6e50c39d01f8a6055fff76761d982
AnorldoM23 <arnoldomunoz23@yahoo.com>
2016-08-22 11:39:20 -0700

    refactor to all Entity tags in index.js

--- index.js
+++ index.js
@@ -1,68 +1,94 @@
-import React, { Component } from 'react'
-import { render } from 'react-dom'
 import 'aframe';
-import {Entity, Scene} from 'aframe-react';
+import {Animation, Entity, Scene} from 'aframe-react';
 
-class HelloWorld extends React.Component {
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class BoilerplateScene extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: 'red'
+    }
+  }
+
+  //var changeColor = function(){ }
+  changeColor () {
+    const colors = ['red', 'orange', 'yellow', 'green', 'blue'];
+    this.setState({
+      color: colors[Math.floor(Math.random() * colors.length)],
+    });
+  };
+
   render () {
     return (
-    	<Scene>
+      <Scene>
+        <Entity light={{type: 'ambient', color: '#888'}}/>
+        <Entity light={{type: 'directional', intensity: 0.5}} position={[-1, 1, 0]}/>
+        <Entity light={{type: 'directional', intensity: 1}} position={[1, 1, 0]}/>
 
-      <a-entity position="0 0 8" camera="userHeight: 1.8" look-controls='' wasd-controls='' ></a-entity>
+      <Entity>
+        <Entity geometry="primitive: plane; width: 30; height: 20" 
+                material={{color: "black"}}
+                position="0 -2 0"
+                rotation="-90 0 0">
+        </Entity>
+
+        <Entity geometry="primitive: box; width: 0.5; height: 10; depth: 10" 
+                material={{color: "orange"}}
+                position="-10 0 -10"
+                rotation="0 90 0">
+        </Entity>
+
+        <Entity geometry="primitive: cylinder; openEnded: true; thetaLength: 180" 
+                material="color: red; side: double"
+                position="0 0 -10"
+                rotation="0 90 0"
+                radius="5"
+                scale="5 10 5">
+        </Entity>
+
+        <Entity geometry="primitive: box; width: 0.5; height: 10; depth: 10" material={{color: "orange"}}
+                position="10 0 -10"
+                rotation="0 90 0">
+        </Entity>
 
 
-      <a-entity id="building">
+        <Entity geometry="primitive: box; width: 0.5; height: 10; depth: 30" material={{color: "#4CC3D9"}}
+                position="0 0 10"
+                rotation="0 90 0">
+        </Entity>
+
+        <Entity geometry="primitive: box; width: 0.5; height: 10; depth: 20" material={{color: "#4CC3D9"}}
+                position="-15 0 0"
+                rotation="0 0 0">
+        </Entity>
+
+        <Entity geometry="primitive: box; width: 0.5; height: 10; depth: 20" material={{color: "#4CC3D9"}}
+                position="-15 0 0"
+                rotation="0 0 0">
+        </Entity>
+
+        <Entity geometry="primitive: box; width: 0.5; height: 10; depth: 20" material={{color: "#4CC3D9"}}
+                position="15 0 0"
+                rotation="0 0 0">
+        </Entity>
+
+        <Entity geometry="primitive: box" material={{color: this.state.color}}
+                onClick={this.changeColor}
+                position="0 1 -5">
+          <Animation attribute="rotation" dur="5000" repeat="indefinite" to="0 360 360"/>
+        </Entity>
+
+      </Entity>
 
         
-        <a-plane position="0 0 0" rotation="-90 0 0" width="30" height="30" color="grey"></a-plane>
 
-     
-        <a-box position="0 5 0" rotation="-90 0 0" width="30" height="30" color="grey"></a-box>
-
-       
-        <a-box id="farWallLeft" position="-10 0 -10" rotation="0 90 0" width="0.5" height="10" depth="10" color="orange"></a-box>
-
-        <a-entity id="farWallCurved" position="0 0 -10" rotation="0 90 0" scale="5 10 5" radius="5" geometry="primitive: cylinder; openEnded: true; thetaLength: 180" material="side: double; color: red"></a-entity>
-
-        <a-box id="farWallRight" position="10 0 -10" rotation="0 90 0" width="0.5" height="10" depth="10" color="orange"></a-box>
-
-       
-        <a-box id="nearWall" position="0 0 10" rotation="0 90 0" width="0.5" height="10" depth="30" color="#4CC3D9"></a-box>
-
-     
-        <a-box id="leftWall" position="-15 0 0" rotation="0 0 0" width="0.5" height="10" depth="20" color="#4CC3D9"></a-box>
-
-       
-        <a-box id="rightWall" position="15 0 0" rotation="0 0 0" width="0.5" height="10" depth="20" color="#4CC3D9"></a-box>
-
-  
-
-        <a-box id="innerWallLeft" position="-11.75 0 0" rotation="0 90 0" width="0.5" height="10" depth="7.5" color="#4CC3D9"></a-box>
-
-       
-
-        <a-box id="innerWallRight" position="11.75 0 0" rotation="0 90 0" width="0.5" height="10" depth="7.5" color="#4CC3D9"></a-box>
-
-      </a-entity>
-
-   
-
-      
-      <a-entity >
-      
-        <a-sphere id="globe" color="blue" position="0 3 0" radius="1.75">
-          
-        </a-sphere>
-
-      </a-entity>
-
-      <a-cylinder id="centerPedestal" position="0 0 0" radius="1.5" height="1.5" color="#8A2BE2"></a-cylinder>
-
-      <a-sky color="#ECECEC"></a-sky>
-
-    </Scene>
-          );
+      </Scene>
+    );
   }
 }
 
-render(<HelloWorld/>, document.getElementById('app'))
+ReactDOM.render(<BoilerplateScene/>, document.querySelector('.scene-container'));
