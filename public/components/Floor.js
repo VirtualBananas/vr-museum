deef1ff270857aefc0fe27c42c936c9b5ae90316
Jorge <jcarapia83@Gmail.com>
2016-08-24 16:34:09 -0700

    added functionality to travel in/out of 360 pics

--- public/components/Floor.js
+++ public/components/Floor.js
@@ -5,7 +5,7 @@ const Floor = ({material}) => {
 
 	return (
 		<Entity>
-			<Entity geometry="primitive: plane; width: 60; height: 40" 
+			<Entity geometry="primitive: plane; width: 60; height: 50" 
         material={material}
         position="0 -2 0"
         rotation="-90 0 0">
