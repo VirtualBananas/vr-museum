03e148de51994312467e7655e978d1e581e273c7
AnorldoM23 <arnoldomunoz23@yahoo.com>
2016-09-01 11:30:34 -0700

    added webrtc voice chat

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import 'aframe';
 import 'babel-polyfill';
 import {Animation, Entity, Scene} from 'aframe-react';
 
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 import Camera from './public/components/Camera';
@@ -15,9 +15,11 @@ import Light from './public/components/Light';
 import Floor from './public/components/Floor';
 import SphereClickPads from './public/components/SphereClickPad'
 import Placards from './public/components/Placards';
-        //<Placards changeCamView = { this.camViewdif.bind(this) } />
+import io from 'socket.io-client/socket.io';
 
-class BoilerplateScene extends React.Component {
+var socket = io();     
+
+class BoilerplateScene extends Component {
 
   constructor(props) {
     super(props);
@@ -32,11 +34,144 @@ class BoilerplateScene extends React.Component {
 
   }
 
+  componentDidMount(){
+    // testing socket io connection
+    socket.on('news', (data) => {
+        console.log(data);
+        socket.emit('my other event', {
+            my: 'Im out here'
+        });
+    })
+
+
+    var userOneVoice = document.getElementById('userOneVoice');
+    var userTwoVoice = document.getElementById('userTwoVoice');
+    var SIGNAL_ROOM = 'signal_room';
+    var ROOM = 'chat'
+    var rtcPeerConn; 
+
+    var configuration = {
+      'iceServers': [{
+        'url': 'stun:stun.l.google.com:19302'
+      }]
+    };
+
+    
+    // join the rooms
+    socket.emit('ready', {
+        "chat_room": ROOM,
+        "signal_room": SIGNAL_ROOM
+    });
+    // emit signal to start call
+    socket.emit('signal', {
+        "type": "user_here", 
+        "message": 'Are you ready for a call?', 
+        "room": SIGNAL_ROOM
+    });
+
+    socket.on('signaling_message', function(data){
+        console.log('data in signaling_message', data.type);
+        if (!rtcPeerConn) {
+            startSignaling();
+        } 
+        if (data.type !== "user_here") {
+            var message = JSON.parse(data.message);
+            if (message.sdp) {
+                rtcPeerConn.setRemoteDescription(new RTCSessionDescription(message.sdp), function(){
+                    if (rtcPeerConn.remoteDescription.type === 'offer') {
+                        rtcPeerConn.createAnswer(sendLocalDesc, logError);
+                    }
+                }, logError)
+            }else{
+                rtcPeerConn.addIceCandidate(new RTCIceCandidate(message.candidate));
+            }
+        }
+    });
+
+    function startSignaling(){
+        console.log("startSignaling signaling....");
+        rtcPeerConn = new webkitRTCPeerConnection(configuration);
+        rtcPeerConn.onicecandidate = function(e){
+            e.preventDefault();
+            if(e.candidate){
+                socket.emit('signal', {
+                    "type": "ice candidate",
+                    "message": JSON.stringify({
+                        'candidate': e.candidate
+                    }),
+                    "room": SIGNAL_ROOM
+                });
+            }
+            console.log("completed that ice canditate....");
+        }
+
+        rtcPeerConn.onnegotiationneeded = function(){
+            console.log("on negotiation called");
+            rtcPeerConn.createOffer(sendLocalDesc, logError);
+        };
+
+        rtcPeerConn.onaddstream = function(e){
+            console.log("going to add their stream");
+            userTwoVoice.src = URL.createObjectURL(e.stream);
+        };
+        
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+        navigator.getUserMedia({
+            video: false,
+            audio: true
+        }, function(stream){
+            console.log("going to display my stream...");
+            userOneVoice.src = URL.createObjectURL(stream);
+            rtcPeerConn.addStream(stream);
+        }, logError);
+    };
+
+
+
+     function sendLocalDesc (desc) {
+        rtcPeerConn.setLocalDescription(desc, function(){
+            console.log("sending local description");
+            socket.emit("signal", {
+                "type": "SDP",
+                "message": JSON.stringify({
+                    'sdp': rtcPeerConn.localDescription
+                }),
+                "room": SIGNAL_ROOM
+            });
+        }, logError);
+      };
+
+      function logError(error){
+        console.log(error.name + ": " + error.message);
+      };
+
+      socket.on('annouce', function(data){
+        console.log(data.message);
+      });
+
+      socket.on('message', function(data){
+        console.log(data.author + ": " + data.message);
+      });
+
+
+
+
+
+  };
+
+
+
+
   render () {
     return (
       <Scene >
-
+      <video id="userOneVoice" autoPlay ></video>
+        <video id="userTwoVoice" autoPlay ></video>
       <a-assets>
+
+       
+
         <img id="world-texture" src="./assets/world.jpg" />
         <img id="floor-texture" src="./assets/image2.jpg" />
         <img id="ceiling-texture" src="./assets/color-rush.png" />
@@ -62,7 +197,7 @@ class BoilerplateScene extends React.Component {
 
         <img id="riceTerrace_pano" src="./assets/riceTerrace_pano.jpg" />
 
-        <video id="diving" autoplay loop="true" src="./assets/360diving.mp4" />
+        
 
       </a-assets>
 
@@ -181,7 +316,7 @@ class BoilerplateScene extends React.Component {
         </Entity>
 
         <Entity geometry="primitive: sphere" 
-                material="src: #diving; side: double"
+                material="src: #pano; side: double"
                 position="-25 2.5 10"
                 radius="1"
                 scale="1.25 1.25 1.25">
@@ -237,7 +372,12 @@ class BoilerplateScene extends React.Component {
         <Navigator changeCamView={ this.camViewdif.bind(this) } />
 
       </Entity>
+
     </Scene>
+
+
+    
+
     );
   }
 }
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 var path = require('path');
 var httpProxy = require('http-proxy');
 var publicPath = path.resolve(__dirname, 'public');
@@ -7,11 +10,13 @@ var port = process.env.PORT || 3000;
 var User = require('./db/models/usermodel');
 
 // We need to add a configuration to our proxy server,
+
 // as we are now proxying outside localhost
 var proxy = httpProxy.createProxyServer({
   changeOrigin: true
 });
-// connection for production
+
+// connection for production database
 mongoose.connect('mongodb://museum:museum12345@ds043027.mlab.com:43027/vrmuseum');
 
 
@@ -19,16 +24,47 @@ mongoose.connect('mongodb://museum:museum12345@ds043027.mlab.com:43027/vrmuseum'
 // mongoose.connect('mongo://localhost/users');
 
 
-var app = express();
-
-//serving our index.html
 app.use(express.static(publicPath));
 
-//server/compiler.js runs webpack-dev-server which creates the bundle.js which index.html serves
-//the compiler adds some console logs for some extra sugar
+
+
 //notice that you will not see a physical bundle.js because webpack-dev-server runs it from memory
 var bundle = require('./server/compiler.js')
 bundle()
+
+
+io.on('connection', function(socket){
+
+	socket.emit('news', { hello: 'World io' });
+
+	socket.on('my other event', function (data) {
+  console.log(data);
+	});
+
+	socket.on('ready', function(data){
+  	socket.join(data.chat_room);
+  	socket.join(data.signal_room);
+
+  	socket.to(data.chat_room).broadcast.emit('announce', {
+  		message: 'New client in the ' + data.chat_room + 'room.'
+  	});
+
+    socket.to(data.signal_room).broadcast.emit('announce', {
+      message: 'New client in the ' + data.signal_room + 'room.'
+    });
+  });
+
+  socket.on('signal', function(data){
+  	// note the user of req here for broadcasting so only the sender doesn't receive ther own messages. We are using req instead of app
+  	console.log("testing for the data inside of signal", data)
+  	
+  	socket.to(data.room).broadcast.emit('signaling_message', {
+  		type: data.type,
+  		message: data.message
+  	})
+  });
+
+})
 
 //express now processes all requests to localhost:8080
 //app.all is a special routing method used for loading middleware functions
@@ -42,6 +78,6 @@ proxy.on('error', function(e) {
   console.log('Could not connect to proxy, please try again...')
 });
 
-app.listen(port, function () {
+server.listen(port, function () {
   console.log('Server running on port ' + port)
 });
