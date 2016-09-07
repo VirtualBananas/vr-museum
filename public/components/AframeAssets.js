10e241de3441d2973a4913605fbd4f677ecd87d3
Ralston Louie <ralstonlouie@gmail.com>
2016-09-07 15:29:53 -0700

    added photos to walls

--- public/components/AframeAssets.js
+++ public/components/AframeAssets.js
@@ -9,6 +9,39 @@ export default () => {
       <img id="floor-texture" src="./assets/image2.jpg" />
       <img id="ceiling-texture" src="./assets/color-rush.png" />
       <img id="wood-texture" src="./assets/wood_1.jpg" />
+
+      <img id="ancient1" src="./assets/photos/ancient_1.jpg" />
+      <img id="ancient2" src="./assets/photos/ancient_2.jpg" />
+      <img id="ancient3" src="./assets/photos/ancient_3.png" />
+      <img id="ancient4" src="./assets/photos/ancient_4.jpg" />
+      <img id="ancient5" src="./assets/photos/ancient_5.jpg" />
+      <img id="ancient6" src="./assets/photos/ancient_6.jpg" />
+      <img id="ancient7" src="./assets/photos/ancient_7.jpg" />
+      <img id="ancient8" src="./assets/photos/ancient_8.jpg" />
+      <img id="ancient10" src="./assets/photos/ancient_10.jpg" />
+
+      <img id="ancientPano" src="./assets/photos/ancient_pano.jpg" />
+
+      <img id="renaissance1" src="./assets/photos/renaissance_1.jpg" />
+      <img id="renaissance2" src="./assets/photos/renaissance_2.jpg" />
+      <img id="renaissance3" src="./assets/photos/renaissance_3.jpg" />
+      <img id="renaissance4" src="./assets/photos/renaissance_4.jpg" />
+      <img id="renaissance5" src="./assets/photos/renaissance_5.jpg" />
+
+      <img id="WWII-1" src="./assets/photos/WWII_1.gif" />
+      <img id="WWII-2" src="./assets/photos/WWII_2.jpg" />
+      <img id="WWII-3" src="./assets/photos/WWII_3.jpg" />
+      <img id="WWII-4" src="./assets/photos/WWII_4.jpg" />
+      <img id="WWII-5" src="./assets/photos/WWII_5.jpg" />
+
+      <img id="modern1" src="./assets/photos/modern_1.jpg" />
+      <img id="modern2" src="./assets/photos/modern_2.jpg" />
+      <img id="modern3" src="./assets/photos/modern_3.jpg" />
+      <img id="modern4" src="./assets/photos/modern_4.jpg" />
+      <img id="modern5" src="./assets/photos/modern_5.jpg" />
+
+      <img id="modernPanoInt" src="./assets/photos/modern_pano_int.jpg" />
+
 		</div>
 	)
 }
--- public/components/Pictures.js
+++ public/components/Pictures.js
@@ -4,34 +4,42 @@ import React from 'react';
  
 const allPictures = [
 	{
-		geometry: "primitive: box; width: 5; height: 5; depth: 0.5",
-		material: "src: #yosemite1", 
+		geometry: "primitive: box; width: 7.5; height: 5; depth: 0.5",
+		material: "src: #ancient10", 
 		position: "22 1.6 0.3", 
 		rotation:"0 0 0"
 	}, 
-	{ geometry: "primitive: box; width: 5; height: 5; depth: 0.5", material:"src: #yosemite2", position: "29.7 1.6 6", rotation: "0 90 0"
+	{ geometry: "primitive: box; width: 5; height: 5; depth: 0.5", material:"src: #ancient2", position: "29.7 1.6 6", rotation: "0 90 0"
 	},
-	{ geometry: "primitive: box; width: 5; height: 5; depth: 0.5", material: "src: #yosemite3", position: "29.7 1.6 14", rotation: "0 90 0"
+	{ geometry: "primitive: box; width: 5; height: 5; depth: 0.5", material: "src: #ancient8", position: "29.7 1.6 14", rotation: "0 90 0"
 	}, 
-	{ geometry: "primitive: box; width: 5; height: 5; depth: 0.5", material: "src: #yosemite4", position: "22 1.6 19.7", rotation: "0 0 0"
+	{ geometry: "primitive: box; width: 8; height: 5; depth: 0.5", material: "src: #ancient6", position: "21 1.6 19.7", rotation: "0 0 0"
 	},  
-	{ geometry: "primitive: box; width: 7.5; height: 5; depth: 0.5", material: "src: #africa1", position: "-22 1.6 19.7", rotation: "0 0 0"
+	{ geometry: "primitive: box; width: 8; height: 5; depth: 0.5", material: "src: #renaissance1", position: "-21 1.6 19.7", rotation: "0 0 0"
 	}, 
-	{ geometry: "primitive: box; width: 7.5; height: 5; depth: 0.5", material:"src: #africa2", position: "-29.7 1.6 5", rotation: "0 90 0"
+	{ geometry: "primitive: box; width: 7.5; height: 5; depth: 0.5", material:"src: #renaissance2", position: "-29.7 1.6 5", rotation: "0 90 0"
 	}, 
-	{ geometry: "primitive: box; width: 7.5; height: 5; depth: 0.5", material:"src: #africa3", position: "-29.7 1.6 15", rotation: "0 90 0"
+	{ geometry: "primitive: box; width: 7.5; height: 5; depth: 0.5", material:"src: #renaissance3", position: "-29.7 1.6 15", rotation: "0 90 0"
 	}, 
-	{ geometry: "primitive: box; width: 7.5; height: 5; depth: 0.5", material:"src: #africa5", position: "-22 1.6 0.3", rotation: "0 0 0"
+	{ geometry: "primitive: box; width: 7.5; height: 5; depth: 0.5", material:"src: #renaissance4", position: "-22 1.6 0.3", rotation: "0 0 0"
 	}, 
-	{ geometry: "primitive: box; width: 10; height: 5; depth: 0.5", material:"src: #china2", position: "23.5 0 0", rotation: "0 0 0"
+	{ geometry: "primitive: box; width: 10; height: 5; depth: 0.5", material:"src: #WWII-1", position: "-20 1.6 -19.7", rotation: "0 0 0"
 	}, 
-	{ geometry: "primitive: box; width: 7.5; height: 5; depth: 0.5", material:"src: #china1", position: "23.5 0 0", rotation: "0 90 0"
+	{ geometry: "primitive: box; width: 7.5; height: 5; depth: 0.5", material:"src: #WWII-2", position: "-29.7 1.6 -5", rotation: "0 90 0"
 	}, 
-	{ geometry: "primitive: box; width: 7.5; height: 5; depth: 0.5", material:"src: #china3", position: "23.5 0 0", rotation: "0 90 0"
+	{ geometry: "primitive: box; width: 7.5; height: 5; depth: 0.5", material:"src: #WWII-3", position: "-29.7 1.6 -15", rotation: "0 90 0"
 	}, 
-	{ geometry: "primitive: box; width: 11.5; height: 5; depth: 0.5", material:"src: #china4", position: "23.5 0 0", rotation: "0 0 0"
+	{ geometry: "primitive: box; width: 10; height: 5; depth: 0.5", material:"src: #WWII-5", position: "-22 1.6 -0.3", rotation: "0 0 0"
 	}, 
-	{ geometry: "primitive: box; width: 20; height: 5; depth: 0.5", material:"src: #serengeti", position: "0 1.6 19.7", rotation: "0 0 0"
+	{ geometry: "primitive: box; width: 10; height: 5; depth: 0.5", material:"src: #modern1", position: "20 1.6 -19.7", rotation: "0 0 0"
+	}, 
+	{ geometry: "primitive: box; width: 7.5; height: 5; depth: 0.5", material:"src: #modern2", position: "29.7 1.6 -5", rotation: "0 90 0"
+	}, 
+	{ geometry: "primitive: box; width: 7.5; height: 5; depth: 0.5", material:"src: #modern3", position: "29.7 1.6 -15", rotation: "0 90 0"
+	}, 
+	{ geometry: "primitive: box; width: 10; height: 5; depth: 0.5", material:"src: #modern4", position: "22 1.6 -0.3", rotation: "0 0 0"
+	},
+	{ geometry: "primitive: box; width: 25; height: 5; depth: 0.5", material:"src: #ancientPano", position: "0 1.6 19.7", rotation: "0 0 0"
 	}
 ];
 
@@ -51,7 +59,7 @@ const Pictures = ({ material}) => {
 	return (
 		<Entity>
 			<Entity geometry="primitive: cylinder; openEnded: true; thetaLength: 180" 
-        material="side: double; src: #riceTerrace_pano"
+        material="side: double; src: #modernPanoInt"
         position="0 1.6 -19.7"
         rotation="0 90 0"
         radius="5"
