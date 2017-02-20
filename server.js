4c48e4077a9f88d1fb7dc88fe8dae956dd2c7693
AnorldoM23 <arnoldomunoz23@yahoo.com>
2017-02-20 13:04:10 -0800

    cleaning server

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
 var express = require('express');
 var app = express();
-var server = require('http').Server(app);
 var path = require('path');
-var httpProxy = require('http-proxy');
+// var httpProxy = require('http-proxy');
 var publicPath = path.resolve(__dirname, 'public');
 var port = process.env.PORT || 3000;
 
 
 // as we are now proxying outside localhost
-var proxy = httpProxy.createProxyServer({
-  changeOrigin: true
-});
-
+// var proxy = httpProxy.createProxyServer({
+//   changeOrigin: true
+// });
 
 app.use(express.static(publicPath));
 
 //notice that you will not see a physical bundle.js because webpack-dev-server runs it from memory
 // var bundle = require('./server/compiler.js')
 // bundle()
-
-
 
 // //express now processes all requests to localhost:8080
 // //app.all is a special routing method used for loading middleware functions
@@ -32,7 +28,7 @@ app.use(express.static(publicPath));
 // proxy.on('error', function(e) {
 //   console.log('Could not connect to proxy, please try again...')
 // });
-
+var server = require('http').Server(app);
 server.listen(port, function () {
   console.log('Server running on port ' + port)
 });
